68d80f69aeb7be07dbd4f705d3a6e7bfc38389b2
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-21 21:58:49 -0400

    Stack top method

--- Stack.js
+++ Stack.js
@@ -17,3 +17,10 @@ Stack.prototype.pop = function(){
     }
     return this.items.pop();
 }
+
+Stack.prototype.top = function(){
+    if(this.length > 0){
+        return this.items[this.length-1];
+    }
+    return undefined;
+}
