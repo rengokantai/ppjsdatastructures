6cadc9ebdabbd4882a3644a4bfcec24b74de55c8
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-26 22:20:17 -0400

    BTree add method

--- BTree.js
+++ BTree.js
@@ -12,3 +12,30 @@ function BTree(){
     this.root = null;
     this.length = 0;
 }
+
+BTree.prototype.add = function(data){
+    var node = new Node(data);
+    this.length = 1;
+
+    if(this.root == null){
+        return this.root = node;
+    }
+
+    var currentNode = this.node;
+    var parentNode = null;
+
+    while(currentNode){
+        parentNode = currentNode;
+        if(data.id < currentNode.data.id){
+            currentNode = currentNode.left;
+            if(currentNode ===null){
+                return parentNode.left = node;
+            }
+        }else{
+            currentNode = currentNode.right;
+            if(currentNode ===null){
+                return parentNode.right = node;
+            }
+        }
+    }
+}
