b068b5c20466a4d1b25a4535f5eae3cbde0bda20
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-29 10:12:34 -0400

    bubbleSort class

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Hernan Y.Ke on 2015/10/29.
+ */
+function bubbleSort(data){
+    var len = data.length;
+    do{
+        for(var i =0; i<len -1;i++){
+            if(data[i]>data[i+1]){
+                swap(data,i,i+1);
+            }
+        }
+    }while(len--);
+    return data;
+}
+
+
+
+function swap(arr, indexA, indexB){
+    var tmp = arr[indexA];
+    arr[indexA]=arr[indexB];
+    arr[indexB]=tmp;
+}
