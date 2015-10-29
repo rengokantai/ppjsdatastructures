b0a99bb58676df22e8e237397afbf17e367dd351
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-28 22:44:34 -0400

    Graph addEdge method

--- Graph.js
+++ Graph.js
@@ -16,3 +16,19 @@ Graph.prototype.addNode = function(node){
     this.nodes.push(node);
     return ++this.length;
 }
+
+
+Graph.prototype.addEdge = function(nodeA, nodeB){
+    var nA = this.nodes.filter(function(node){
+        return node.data.id ===nodeA;
+    });
+
+    var nB = this.nodes.filter(function(node){
+        return node.data.id ===nodeB;
+    });
+
+    if(nA.length && nB.length){
+        nA[0].neighbours.push(nB[0]);
+        nB[0].neighbours.push(nA[0]);
+    }
+}
