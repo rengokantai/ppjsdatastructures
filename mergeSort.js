f5ac125202f438dc5f7538575248f5b1fa392554
Hernan Y.Ke <yuriqiao@gmail.com>
2015-10-31 15:29:01 -0400

    mergeSort class

---
+++ mergeSort.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Hernan Y.Ke on 2015/10/31.
+ */
+function mergeSort(data){
+    if(data.length < 2){
+        return data;
+    }
+
+    var midPoint = Math.round(data.length / 2);
+
+    return merge(
+        mergeSort(data.slice(0,midPoint)),
+        mergeSort(data.slice(midPoint))
+    );
+}
+
+
+function merge(left,right){
+    var out =[];
+    while(left.length &&right.length){
+        out.push(left[0]<right[0]?left.shift():right.shift());
+    }
+    while(left.length){
+        out.push(left.shift());
+    }
+    while(right.length){
+        out.push(right.shift())
+    }
+    return out;
+}
